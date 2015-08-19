7e96aa7440d3ff333a192e6e210af3031671c6c8
Galya <glifada@mail.bg>
2015-08-19 20:23:38 +0300

    Initial Commit

---
+++ High-Quality-Code/13. DevelopmentToolsHomework/01. ResearchAndUseDevelopmentTools/OOPPrinciplesPart2/01.Shapes/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Shapes
+{
+    class Square : Shape
+    {
+        public Square (double width)
+            : base(width, width)
+        {
+
+        }
+
+        public override double CalculateSurface()
+        {
+            return this.Width * this.Width;
+        }
+    }
+}
